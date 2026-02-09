36c7a857882e26a99e850e924a08409a44efdc27
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:12:00 -0500

    se cambia el hola mundo por vamos a sumar dos numeros

--- nacholee.java
+++ nacholee.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class nacholee {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("vamos a sumar dos numeros");
+        int n1=0, n2=0,suma=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese el numero");
+        n1=sc.nextInt();
+        System.out.println("ingrese el numero 2");
+        n2= sc.nextInt();
+        System.out.println("la suma de los numeros es: "+(n1+n2));
     }
 }
